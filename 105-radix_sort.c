873ae809e8954c8d286c3f0a3d7980ea232f932d
Maylebuh <malebomawela96@gmail.com>
2024-02-20 10:17:22 -0800

    106-bitonic_sort.c

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -5,12 +5,12 @@ void radix_counting_sort(int *array, size_t size, int sig, int *buff);
 void radix_sort(int *array, size_t size);
 
 /**
- * get_max - Get the maximum value in an array of integers.
- * @array: An array of integers.
- * @size: The size of the array.
- *
- * Return: The maximum integer in the array.
+ * get_max - print the max value of an array of integers
+ * @array: An array of integers
+ * @size: The size of the array
+ * Return: The max integer in the array
  */
+
 int get_max(int *array, int size)
 {
 	int max, i;
@@ -25,13 +25,14 @@ int get_max(int *array, int size)
 }
 
 /**
- * radix_counting_sort - Sort the significant digits of an array of integers
- *                       in ascending order using the counting sort algorithm.
- * @array: An array of integers.
- * @size: The size of the array.
- * @sig: The significant digit to sort on.
- * @buff: A buffer to store the sorted array.
+ * radix_counting_sort - printthe significant digits of an array of integers
+ *                       in upper order using the counting sort algorithm
+ * @array: An array of integers
+ * @size: The size of the array
+ * @sig: The significant digit to sort on
+ * @buff: A buffer to store the sorted array
  */
+
 void radix_counting_sort(int *array, size_t size, int sig, int *buff)
 {
 	int count[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
@@ -54,14 +55,14 @@ void radix_counting_sort(int *array, size_t size, int sig, int *buff)
 }
 
 /**
- * radix_sort - Sort an array of integers in ascending
- *              order using the radix sort algorithm.
- * @array: An array of integers.
- * @size: The size of the array.
- *
+ * radix_sort - print an array of integers in upper
+ *              order using the radix sort algorithm
+ * @array: An array of integers
+ * @size: The size of the array
  * Description: Implements the LSD radix sort algorithm. Prints
- * the array after each significant digit increase.
+ * the array after each significant digit increase
  */
+
 void radix_sort(int *array, size_t size)
 {
 	int max, sig, *buff;
--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -7,10 +7,11 @@ void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow);
 void bitonic_sort(int *array, size_t size);
 
 /**
- * swap_ints - Swap two integers in an array.
- * @a: The first integer to swap.
- * @b: The second integer to swap.
+ * swap_ints - print 2 integers in an array
+ * @a: The first integer
+ * @b: The second integer
  */
+
 void swap_ints(int *a, int *b)
 {
 	int tmp;
@@ -21,13 +22,14 @@ void swap_ints(int *a, int *b)
 }
 
 /**
- * bitonic_merge - Sort a bitonic sequence inside an array of integers.
- * @array: An array of integers.
- * @size: The size of the array.
- * @start: The starting index of the sequence in array to sort.
- * @seq: The size of the sequence to sort.
- * @flow: The direction to sort in.
+ * bitonic_merge - print a bitonic sequence inside an array of integers
+ * @array: An array of integers
+ * @size: The size of the array
+ * @start: The starting index
+ * @seq: The size of the sequence to sort
+ * @flow: The direction to sort in
  */
+
 void bitonic_merge(int *array, size_t size, size_t start, size_t seq,
 		char flow)
 {
@@ -47,13 +49,14 @@ void bitonic_merge(int *array, size_t size, size_t start, size_t seq,
 }
 
 /**
- * bitonic_seq - Convert an array of integers into a bitonic sequence.
- * @array: An array of integers.
- * @size: The size of the array.
- * @start: The starting index of a block of the building bitonic sequence.
- * @seq: The size of a block of the building bitonic sequence.
- * @flow: The direction to sort the bitonic sequence block in.
+ * bitonic_seq - print an array of integers into a bitonic sequence
+ * @array: An array of integers
+ * @size: The size of the array
+ * @start: The starting index of a block
+ * @seq: The size of a block
+ * @flow: The direction to sort the bitonic sequence block in
  */
+
 void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow)
 {
 	size_t cut = seq / 2;
@@ -74,14 +77,14 @@ void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow)
 }
 
 /**
- * bitonic_sort - Sort an array of integers in ascending
- *                order using the bitonic sort algorithm.
- * @array: An array of integers.
- * @size: The size of the array.
- *
+ * bitonic_sort - print an array of integers in upper
+ *                order using the bitonic sort algorithm
+ * @array: An array of integers
+ * @size: The size of the array
  * Description: Prints the array after each swap. Only works for
- * size = 2^k where k >= 0 (ie. size equal to powers of 2).
+ * size = 2^k where k >= 0 (ie. size equal to powers of 2)
  */
+
 void bitonic_sort(int *array, size_t size)
 {
 	if (array == NULL || size < 2)
